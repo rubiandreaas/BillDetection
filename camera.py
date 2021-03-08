3a289f33adc8ade496aa1d6b12be27957b34b562
Andrea Alvarez <rubiandreaas@gmail.com>
2021-03-08 14:19:54 -0600

    beta

--- camera.py
+++ camera.py
@@ -60,6 +60,8 @@ import pygame
 import numpy as np
 import glob
 import datetime as dt
+from goto import with_goto
+import time
 
 MIN_MATCH = 20
 showText = ''
@@ -120,7 +122,7 @@ def match_draw(img1,img2,found,cont,showText,sound):
     good_matches = [m[0] for m in matches
                     if len(m) == 2 and m[0].distance < m[1].distance * ratio]
 
-    print('good matches:%d/%d' % (len(good_matches), len(matches)))
+    #print('good matches:%d/%d' % (len(good_matches), len(matches)))
 
     matchesMask = np.zeros(len(good_matches)).tolist()
 
@@ -128,7 +130,7 @@ def match_draw(img1,img2,found,cont,showText,sound):
     if len(good_matches) > MIN_MATCH:
         found = True
         cont = cont + 1
-
+        #print(cont)
         src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches])
         dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches])
         mtrx, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
@@ -169,11 +171,17 @@ def match_draw(img1,img2,found,cont,showText,sound):
 
     #res = img2
     #cv2.imshow("prueba", res)
+    #res = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None,
+                          #matchesMask=matchesMask,
+                          #flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+    #cv2.putText(res, showText, (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+    #cv2.imshow("hello", res)
     return found, cont
 
 #cargar imágenes y llenar arrays con feautures como referencias para la comparación de imágenes
 #recibe los array de imagenes y features globales
-def load(temp_kp, temp_desc,img):
+def load_images(temp_kp, temp_desc,img):
 
     # cargar la ruta de las imágenes
     filesname = [img for img in glob.glob("bills/*")]
@@ -184,6 +192,7 @@ def load(temp_kp, temp_desc,img):
 
     # recorrer todas las imágenes, calcular y guardar sus features en array
     for i in filesname:
+        print(i)
         image = cv2.imread(i)
         img.append(image)
         t_temp_kp, t_temp_desc = detector.detectAndCompute(image, None)
@@ -195,10 +204,59 @@ detector, matcher = init_feature()
 temp_kp = []
 temp_desc = []
 img = []
+data_bill = []
 
-load(temp_kp, temp_desc,img)
+
+def load_data_set():
+            data_bill_500_1=[img[16],temp_kp[16], temp_desc[16], '500', sound500]
+            data_bill.append(data_bill_500_1)
+            data_bill_50_1=[img[24],temp_kp[24], temp_desc[24], '50', sound50]
+            data_bill.append(data_bill_50_1)
+            data_bill_100_1=[img[0],temp_kp[0], temp_desc[0],'100', sound100]
+            data_bill.append(data_bill_100_1)
+            data_bill_200_1=[img[8],temp_kp[8], temp_desc[8], '200', sound200]
+            data_bill.append(data_bill_200_1)
+            data_bill_20_1=[img[12],temp_kp[12], temp_desc[12], '20', sound20]
+            data_bill.append(data_bill_20_1)
+            data_bill_50_2=[img[25],temp_kp[25], temp_desc[25], '50', sound50]
+            data_bill.append(data_bill_50_2)
+            data_bill_100_2=[img[1],temp_kp[1], temp_desc[1],'100', sound100]
+            data_bill.append(data_bill_100_2)
+            data_bill_200_2=[img[9],temp_kp[9], temp_desc[9], '200', sound200]
+            data_bill.append(data_bill_200_2)
+            data_bill_20_2=[img[13],temp_kp[13], temp_desc[14], '20', sound20]
+            data_bill.append(data_bill_20_2)
+            data_bill_500_2=[img[17],temp_kp[17], temp_desc[17], '500', sound500]
+            data_bill.append(data_bill_500_2)
+            data_bill_50_3=[img[26],temp_kp[26], temp_desc[26], '50', sound50]
+            data_bill.append(data_bill_50_3)
+            data_bill_100_3=[img[2],temp_kp[2], temp_desc[2],'100', sound100]
+            data_bill.append(data_bill_100_3)
+            data_bill_200_3=[img[10],temp_kp[10], temp_desc[10], '200', sound200]
+            data_bill.append(data_bill_200_3)
+            data_bill_20_3=[img[14],temp_kp[14], temp_desc[14], '20', sound20]
+            data_bill.append(data_bill_20_3)
+            data_bill_500_3=[img[18],temp_kp[18], temp_desc[18], '500', sound500]
+            data_bill.append(data_bill_500_3)
+            data_bill_50_4=[img[27],temp_kp[27], temp_desc[27], '50', sound50]
+            data_bill.append(data_bill_50_4)
+            data_bill_100_4=[img[3],temp_kp[0], temp_desc[0],'100', sound100]
+            data_bill.append(data_bill_100_4)
+            data_bill_200_4=[img[11],temp_kp[11], temp_desc[11], '200', sound200]
+            data_bill.append(data_bill_200_4)
+            data_bill_20_4=[img[15],temp_kp[15], temp_desc[15], '20', sound20]
+            data_bill.append(data_bill_20_4)
+            data_bill_500_4=[img[19],temp_kp[19], temp_desc[19], '500', sound500]
+            data_bill.append(data_bill_500_4)
+
+            
+#Cargar imágenes y crear set de datos
+load_images(temp_kp, temp_desc,img)
+load_data_set()
 
 class VideoCamera(object):
+    
+
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
@@ -211,196 +269,38 @@ class VideoCamera(object):
         self.stream.stop()
 
    
-    
+    @with_goto
     def get_frame(self):
-        
-        img2 = self.stream.read()
-
-        img2 = preprocess(img2)
-
         found = False
         searchIndex = 1
         cont = 0
         sound = ''
+        showText = ''
+        img1 = img[0]
 
-        #comienza la comparación para detectar el billete
-        if not found:
-                if searchIndex <= 28:
-                    if searchIndex == 1:
-                        img1 = img[0]
-                        kp1 = temp_kp[0]
-                        desc1 = temp_desc[0]
-                        showText = '100'
-                    elif searchIndex == 2:
-                        img1 = img[1]
-                        kp1 = temp_kp[1]
-                        desc1 = temp_desc[1]
-                        showText = '100'
-                    elif searchIndex == 3:
-                        img1 = img[2]
-                        kp1 = temp_kp[2]
-                        desc1 = temp_desc[2]
-                        showText = '100'
-                    elif searchIndex == 4:
-                        img1 = img[3]
-                        kp1 = temp_kp[3]
-                        desc1 = temp_desc[3]
-                        showText = '100'
-                    elif searchIndex == 5:
-                        img1 = img[4]
-                        kp1 = temp_kp[4]
-                        desc1 = temp_desc[4]
-                        showText = '100'
-                    elif searchIndex == 6:
-                        img1 = img[5]
-                        kp1 = temp_kp[5]
-                        desc1 = temp_desc[5]
-                        showText = '100'
-                    elif searchIndex == 7:
-                        img1 = img[6]
-                        kp1 = temp_kp[6]
-                        desc1 = temp_desc[6]
-                        showText = '100'
-                    elif searchIndex == 8:
-                        img1 = img[7]
-                        kp1 = temp_kp[7]
-                        desc1 = temp_desc[7]
-                        showText = '100'
+        img2 = self.stream.read()
+        img2 = preprocess(img2)
 
-                    #200
-                    elif searchIndex == 9:
-                        img1 = img[8]
-                        kp1 = temp_kp[8]
-                        desc1 = temp_desc[8]
-                        showText = '200'
-                    elif searchIndex == 10:
-                        img1 = img[9]
-                        kp1 = temp_kp[9]
-                        desc1 = temp_desc[9]
-                        showText = '200'
-                    elif searchIndex == 11:
-                        img1 = img[10]
-                        kp1 = temp_kp[10]
-                        desc1 = temp_desc[10]
-                        showText = '200'
-                    elif searchIndex == 12:
-                        img1 = img[11]
-                        kp1 = temp_kp[11]
-                        desc1 = temp_desc[11]
-                        showText = '200'
+        
+        while(True):
 
-                    #20
-                    elif searchIndex == 13:
-                        img1 = img[12]
-                        kp1 = temp_kp[12]
-                        desc1 = temp_desc[12]
-                        showText = '20'
-                    elif searchIndex == 14:
-                        img1 = img[13]
-                        kp1 = temp_kp[13]
-                        desc1 = temp_desc[13]
-                        showText = '20'
-                    elif searchIndex == 15:
-                        img1 = img[14]
-                        kp1 = temp_kp[14]
-                        desc1 = temp_desc[14]
-                        showText = '20'
-                    elif searchIndex == 16:
-                        img1 = img[15]
-                        kp1 = temp_kp[15]
-                        desc1 = temp_desc[15]
-                        showText = '20'
+            #comienza la comparación para detectar el billete
+            for bill in data_bill:
+                  
+                img1 = bill[0]
+                kp1 = bill[1]
+                desc1 = bill[2]
+                showText = bill[3]
+                sound = bill[4]
+                print(showText)
+                         
+                found, cont = match_draw(img1, img2, found, cont, showText,sound)
 
-                    #500
-                    elif searchIndex == 17:
-                        img1 = img[16]
-                        kp1 = temp_kp[16]
-                        desc1 = temp_desc[16]
-                        showText = '500'
-                    elif searchIndex == 18:
-                        img1 = img[17]
-                        kp1 = temp_kp[17]
-                        desc1 = temp_desc[17]
-                        showText = '500'
-                    elif searchIndex == 19:
-                        img1 = img[18]
-                        kp1 = temp_kp[18]
-                        desc1 = temp_desc[18]
-                        showText = '500'
-                    elif searchIndex == 20:
-                        img1 = img[19]
-                        kp1 = temp_kp[19]
-                        desc1 = temp_desc[19]
-                        showText = '500'
-                    elif searchIndex == 21:
-                        img1 = img[20]
-                        kp1 = temp_kp[20]
-                        desc1 = temp_desc[20]
-                        showText = '500'
-                    elif searchIndex == 22:
-                        img1 = img[21]
-                        kp1 = temp_kp[21]
-                        desc1 = temp_desc[21]
-                        showText = '500'
-                    elif searchIndex == 23:
-                        img1 = img[22]
-                        kp1 = temp_kp[22]
-                        desc1 = temp_desc[22]
-                        showText = '500'
-                    elif searchIndex == 24:
-                        img1 = img[23]
-                        kp1 = temp_kp[23]
-                        desc1 = temp_desc[23]
-                        showText = '500'
-
-                    #50                
-                    elif searchIndex == 25:
-                        img1 = img[24]
-                        kp1 = temp_kp[24]
-                        desc1 = temp_desc[24]
-                        showText = '50'
-                    elif searchIndex == 26:
-                        img1 = img[25]
-                        kp1 = temp_kp[25]
-                        desc1 = temp_desc[25]
-                        showText = '50'
-
-                    elif searchIndex == 27:
-                        img1 = img[26]
-                        kp1 = temp_kp[26]
-                        desc1 = temp_desc[26]
-                        showText = '50'
-                    elif searchIndex == 28:
-                        img1 = img[27]
-                        kp1 = temp_kp[27]
-                        desc1 = temp_desc[27]
-                        showText = '50'
-
-                    searchIndex = searchIndex+1
-                else:
-                    showText = '100'
-                    searchIndex = 1
-                    img1 = img[0]
-
-        #escoger el sonido a reproducir 
-        if showText == '20':
-            sound = sound20
-        elif showText == '50':
-            sound = sound50
-        elif showText == '100':
-            sound = sound100
-        elif showText == '200':
-            sound = sound200
-        elif showText == '500':
-            sound = sound500
-
-        found, cont = match_draw(img1, img2, found, cont, showText,sound)
-    
-
-        cv2.putText(img2, showText, (100 - 70, 200 - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
-        ret, jpeg = cv2.imencode('.jpg', img2)
-        data = []
-        data.append(jpeg.tobytes())
-        data.append("found")
-        return data
+                if found:
+                   break
+                
+            ret, jpeg = cv2.imencode('.jpg', img2)
+            data = []
+            data.append(jpeg.tobytes())
+            return data
+                    
