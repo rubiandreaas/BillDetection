1ac7ba4821de3f10cb18b77c34a550c2b6905592
Andrea Alvarez <rubiandreaas@gmail.com>
2021-03-08 18:16:59 -0600

    beta

--- myenv/bin/activate.fish
+++ myenv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rubiandrea/Documentos/udemy/flask/welcome/FaceRecognitionFlask-master/myenv"
+set -gx VIRTUAL_ENV "/home/rubiandrea/Documentos/udemy/flask/welcome/BillDetectionMaster/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
