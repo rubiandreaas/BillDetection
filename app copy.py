b89ab0d1f5fa808abbf44bd2dee53f13a6c82e68
Andrea Alvarez <rubiandreaas@gmail.com>
2021-03-11 11:34:36 -0600

    beta prueba

--- app copy.py
+++ app copy.py
@@ -30,7 +30,6 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
-    
 
 
 if __name__=="__main__":
